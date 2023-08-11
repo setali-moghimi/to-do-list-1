280b345174a0924738339404d248722c510c0766
unknown <setalim7@gmail.com>
2023-08-11 15:37:44 +0330

    upload mini project of to do list 1

---
+++ js/script.js
@@ -0,0 +1,30 @@
+﻿let inp=document.querySelector('.m-auto')
+let ul = document.querySelector('.todos')
+let li=document.querySelectorAll('.fa')
+inp.addEventListener('keydown',function(event){
+    if(event.code=='Enter'){
+        event.preventDefault()
+        if(inp.value!=''){
+
+            ul.innerHTML+='<li class="list-group-item d-flex justify-content-between align-items-center">'
+            +'<span>'+inp.value+'</span>'+
+            '<i class="fa fa-trash-o delete"></i>'
+            +'</li>'
+            inp.value=''
+        }
+        li=document.querySelectorAll('.fa')
+        console.log(li);
+        li.forEach(function(n){
+            console.log(n);
+            n.addEventListener('click',function(){
+                n.parentElement.remove()
+            })
+        })
+    }
+})
+li.forEach(function(n){
+    console.log(n);
+    n.addEventListener('click',function(){
+        n.parentElement.remove()
+    })
+})
